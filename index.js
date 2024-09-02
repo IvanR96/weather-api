9ded703a7681c06bea4eb2c15f0e313f94ea54dd
IvanR96 <irios96@hotmail.com>
2024-09-01 23:34:39 -0700

    api key and url up

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ import express from "express";
 import axios from "axios";
 
 const app = express();
-
 const port = 3000;
+// Define your API key as a constant
+const API_KEY = 'f56f8d2631b45cd9bd5791cb953a3115';
+
+// Define the base URL as a constant
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+
+
+// Combine everything to create the full URL
+const FULL_URL = `${BASE_URL}?q=${CITY_NAME}&appid=${API_KEY}`;
+
+// Now you can use FULL_URL to make your API request
+console.log(FULL_URL);
 
 app.use(express.static('public'));
 
